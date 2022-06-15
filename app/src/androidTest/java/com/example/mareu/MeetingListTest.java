2b79cf0d89a683c26db761b8803e4da1cf44dbeb
JeyJeremazing <jeremy.michee@hotmail.fr>
2022-06-15 14:15:01 +0200

    MeetingList Integration test

--- app/src/androidTest/java/com/example/mareu/MeetingListTest.java
+++ app/src/androidTest/java/com/example/mareu/MeetingListTest.java
@@ -2,12 +2,17 @@ package com.example.mareu;
 
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static com.example.mareu.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -22,7 +27,7 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class MeetingListTest {
 
-    private static int ITEMS_COUNT = 7;
+    private final static int ITEMS_COUNT = 7;
 
     private MeetingsListActivity mActivity;
 
@@ -42,5 +47,23 @@ public class MeetingListTest {
     public void checkTheListIsNotEmpty() {
         onView(ViewMatchers.withId(R.id.recyclerView)).check(matches(hasMinimumChildCount(1)));
 }
+
+    @Test
+    public void removeMeetingItem() {
+        onView(ViewMatchers.withId(R.id.recyclerView)).check(withItemCount(ITEMS_COUNT));
+        onView(ViewMatchers.withId(R.id.recyclerView))
+                .perform(actionOnItemAtPosition(1, new DeleteViewAction()));
+        onView(ViewMatchers.withId(R.id.recyclerView)).check(withItemCount(ITEMS_COUNT-1));
+    }
+
+    @Test
+    public void clickForLaunchTheAddMeetingActivityScreen() {
+        onView(withId(R.id.floatingActionButton)).perform((click()));
+        onView(ViewMatchers.withId(R.id.addMeeting)).check(matches(isDisplayed()));
+    }
+
+
+
+
 
 }
